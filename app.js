9e5f85bc0ecf846f2583004d3390f94ba803f331
Perlou <a87387875@163.com>
2017-01-30 13:16:15 +0800

    init

---
+++ app.js
@@ -0,0 +1,16 @@
+/**
+ * app.js
+ * @author Perlou(perloukevin@gmail.com)
+ */
+
+App({
+    onLaunch: function () {
+        console.log('app on launch')
+    },
+    onShow: function () {
+        console.log('app on show')
+    },
+    onHide: function () {
+        console.log('app on hide')
+    }
+}) 
