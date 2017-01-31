930c0caea1833153c4ce3cfbe89ada0ed5242428
Perlou <a87387875@163.com>
2017-01-31 17:42:36 +0800

    coming

--- pages/coming/coming.js
+++ pages/coming/coming.js
@@ -1,11 +1,11 @@
 /**
- * 热映页
+ * 待映页
  * @author Perlou(perloukevin@gmail.com)
  */
 
 import fetch from '../../common/js/fetch'
 
-const url = 'https://api.douban.com/v2/movie/in_theaters'
+const url = 'https://api.douban.com/v2/movie/coming_soon'
 const searchByTagUrl = 'https://api.douban.com/v2/movie/search?tag='
 
 Page({
